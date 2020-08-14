af54cd9511e89c55b2b75315c3827db91454cc5a
jiyoung <jiyoung4061@naver.com>
2020-08-14 15:22:03 +0900

    Redux with vanilla-js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,29 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO"
 const DELETE_TODO = "DELETE_TODO"
 
+const addToDo = (text) => { //action을 create하는 부분
+    return { //return 부분이 reducer의 action으로 보내짐
+        type: ADD_TODO,
+        text
+    }
+}
+const deleteToDo = (id) => {
+    return {
+        type:DELETE_TODO,
+        id
+    }
+}
+
+//array에서 요소를 delete하는 방법 => splice(하지만 array를 mutate하는 것이기 때문에 X)
+// filter : 테스트를 통과한 모든 element들로 ***새로운 array***를 만듬
 const reducer = (state=[], action) => {
     switch(action.type){
         case ADD_TODO:
-            return [...state, {text: action.text, id: Date.now()}]
+            const newToDoObj = {text: action.text, id: Date.now()}
+            return [newToDoObj, ...state]
         case DELETE_TODO:
-            return []
+            const cleaned = state.filter(toDo=> toDo.id !== action.id)
+            return cleaned;
         default:
             return state;
     }
@@ -22,19 +39,13 @@ const store = createStore(reducer);
 
 store.subscribe(()=> console.log(store.getState()))
 
-const addTodo = (text) => {
-    store.dispatch({type:ADD_TODO, text})
+const dispatchAddToDo = (text) => { //action을 dispatch하기위한 함수
+    store.dispatch(addToDo(text))
 }
 
-const deleteToDo= (e) => {
-    // console.log("e", e.target.parentNode.id);
-    //e.target : click된 해당 버튼
-    //e.target.parentNode : 버튼의 상위 node (li태그)
-    //e.target.parentNode.id : 해당 li태그의 id
-
-    const id = e.target.parentNode.id;
-    store.dispatch({type:DELETE_TODO, id})
-
+const dispatchDeleteToDo= (e) => {
+    const id = parseInt(e.target.parentNode.id);
+    store.dispatch(deleteToDo(id))
 }
 
 const paintToDos = () => {
@@ -44,7 +55,7 @@ const paintToDos = () => {
         const li = document.createElement("li");
         const btn = document.createElement("button");
         btn.innerText = "DEL";
-        btn.addEventListener("click", deleteToDo)
+        btn.addEventListener("click", dispatchDeleteToDo)
         li.id = toDo.id
         li.innerText = toDo.text;
 
@@ -53,14 +64,13 @@ const paintToDos = () => {
     })
 }
 
-store.subscribe(paintToDos)
-
+store.subscribe(paintToDos) //toDo의 변화에따라 list를 re-painting하고 있음
 
 const onSubmit = e => {
     e.preventDefault();
     const toDo = input.value; // input값 받아오기
     input.value="";
-    addTodo(toDo);
+    dispatchAddToDo(toDo);
 }
 
-form.addEventListener("submit", onSubmit);
+form.addEventListener("submit", onSubmit);  
