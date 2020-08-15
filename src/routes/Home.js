6ae4c1859edd026a859244daebab54e40d058e54
jiyoung <jiyoung4061@naver.com>
2020-08-15 14:58:16 +0900

    qwer

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,39 +1,39 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 
-function Home({toDos}) {
-    // console.log("props:", props);
-    const [text, setText] = useState("");
+function Home({ toDos }) {
+  console.log("props:", props);
+  const [text, setText] = useState("qwer");
 
-    function onChange(e) {
-        setText(e.target.value)
-    }
+  function onChange(e) {
+    setText(e.target.value);
+  }
 
-    function onSubmit(e) {
-        e.preventDefault();
-        setText("")
-    }
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+  }
 
-    return (
+  return (
     <>
-    <h1>To Do</h1>
-    <form onSubmit={onSubmit}>
-    <input type="text" value={text} onChange={onChange}/>
-    <button>Add</button>
-    </form>
-    <ul>{JSON.stringify(toDos)}</ul>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
-    )
+  );
 }
 
 //connect()는 Home으로 보내는 props에 추가될수 있도록 허용해줌
 //일반적으로 getCurrentState 함수는 mapStateToProps라고 하는데
 //mapStateToProps는 redux state로 부터 home(component)에 props로써 전달.
 function getCurrentState(state) {
-    return {toDos: state}
-    
-    //return { jiyoung: true}
-    //Home의 props에 jiyoung:true가 추가됨
+  return { toDos: state };
+
+  //return { jiyoung: true}
+  //Home의 props에 jiyoung:true가 추가됨
 }
 
 //getCurrentState함수를 통해 store에서 state를 Home으로 가져다줌
